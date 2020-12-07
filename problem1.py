a4b92811682f4a6bdbf3e40ab11689e1c3cc93d6
Andrew Chan <andrew.chan23@ycdsbk12.ca>
2020-12-07 18:28:41 +0000

    completed problem 1 and 2

--- problem1.py
+++ problem1.py
@@ -1,1 +1,22 @@
+'''
+-------------------------------------------------------------------------
+Name:		problem1.py
 
+Purpose: User is able to input temperature in Celsius and the program will convert the temperature to Fahrenheit
+
+Author:	Chan. A
+
+Created:	date in 12/07/2020
+-------------------------------------------------------------------------
+'''
+# introduction to what program is
+print("-Welcome to the Celsius to Fahrenheit Converter-")
+
+# asks user for input
+celsius = float(input("Please enter your temperature in Celsius (°C): "))
+
+# convert Celsius to Fahrenheit using Celsius to Fahrenheit formula
+convert = (celsius * 9/5) + 32
+
+# print final statement of temperature in Fahrenheit
+print("Temperature in Fahrenheit: " + str(convert) + "°F")
--- problem2.py
+++ problem2.py
@@ -1,1 +1,23 @@
+'''
+-------------------------------------------------------------------------
+Name:		problem2.py
 
+Purpose: User inputs number of students and number of available pieces of Popeyes chicken. The program then calculates how many pieces of student will receive, while Mr. Fabroa receives the remaining pieces of chicken
+
+Author:	Chan. A
+
+Created:	date in 12/07/2020
+-------------------------------------------------------------------------
+'''
+print("Mr. Fabroa's class won a contest!")
+# user inputs number of student
+students = int(input("Enter the number of students in class: "))
+
+# user inputs number of available pieces of chicken
+chicken_pieces = int(input("Enter how many pieces of Popeyes chicken are available?: "))
+
+# how many pieces each student will receive
+print("Each student will get " + str(chicken_pieces//students) + " pieces of Popeyes chicken.")
+
+# how many pieces of chicken Mr. Fabroa will receive
+print("Mr. Fabroa will get " + str(chicken_pieces%students) + " piece(s) of the remaining Popeyes chicken.")
